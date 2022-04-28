9308de34143a70bac7a20b41f5ca436b3bcb2a46
Lauri Fiestas <89028886+PrinssiFiestas@users.noreply.github.com>
2022-04-28 17:18:01 +0300

    Update kSymExport.c

--- kSymExport.c
+++ kSymExport.c
@@ -5,20 +5,20 @@ MODULE_LICENSE("GPL");
 
 int exportteri(void)
 {
-	printk(KERN_ALERT "Paskaa ulos funktiost %s\n", __func__);
+	printk(KERN_ALERT "Viestii ulos funktiost %s\n", __func__);
 	return 0;
 }
 EXPORT_SYMBOL(exportteri);
 
 __init int kModuleInit(void)
 {
-	printk(KERN_ALERT "kSymExport.c T‰‰ funktio siis %s\n", __func__);
+	printk(KERN_ALERT "kSymExport.c T√§√§ funktio siis %s\n", __func__);
 	return 0;
 }
 
 void kModuleExit(void)
 {
-	printk(KERN_ALERT "kSymExport.c T‰‰ funktio on %s\n", __func__);
+	printk(KERN_ALERT "kSymExport.c T√§√§ funktio on %s\n", __func__);
 }
 
 module_init(kModuleInit);
